757264a241d2f2451948b60bbafef2c566bcc6cb
CCDM21 <ccdm21@CCDM21.local>
2018-11-01 10:01:32 -0700

    Javascript cambios

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -1,8 +1,34 @@
 <script>
-	var colorAmarillo = function(x){
-		x.style.background = "yellow";
+	var operando1=document.getElementById('operando1')
+	var operando2=document.getElementById('operando2')
+
+	operando1.addEventListener("focus",colorAmarillo);
+	operando1.addEventListener("focus",colorBlanco); 
+
+	var colorAmarillo = function(){
+		this.style.background = "yellow";
 	}
-	var colorBlanco = function(x){
-		x.style.background = "white";
+	var colorBlanco = function(){
+		this.style.background = "white";
 	}
+
+
+	var operador='';
+	var numeros = function(){
+		var valor=this.value;
+		if (operador=="" ){
+			var valorInput = document.getElementById("operando1").value;
+			if (valorInput==0) {
+				document.getElementById("operando1").value="";
+
+			}
+			document.getElementById("operando1")/value+=valor;
+		}else{
+			//operando2 a;adir
+		}
+	}
+
+	var uno=document.getElementById("uno");
+	uno.addEventListener("click",numeros);
+	
 </script>
